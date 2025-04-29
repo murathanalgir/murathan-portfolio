40e7a19f643b33000f144e4789980714b77cbccc
Murathan <mrthnalgr34@gmail.com>
2025-04-29 22:32:33 +0300

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ThemeProvider, useTheme } from "next-themes";
 
 export default function Providers({children}: {children: React.ReactNode}) {
