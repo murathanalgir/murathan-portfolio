e989f31b2dc94b0aac5d3709d84e4b53922c33e7
Murathan <mrthnalgr34@gmail.com>
2025-05-01 00:32:56 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
         )}
         >
           <div>
-        <noSSR />
         <Providers>
           <Header />
           <main className='grow'>{children}</main>
