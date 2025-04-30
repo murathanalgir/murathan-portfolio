e2a2ba9f0621c501aa6140b78be8282338a202af
Murathan <mrthnalgr34@gmail.com>
2025-05-01 00:31:47 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css'
 import Providers from '@/components/providers'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 const playfair = Playfair_Display({
