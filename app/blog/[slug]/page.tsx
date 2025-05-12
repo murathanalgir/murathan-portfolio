ecf7c949724fb2065c251f9f1851afa497119d47
Murathan <mrthnalgr34@gmail.com>
2025-05-12 22:06:08 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,44 +1,47 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+// app/blog/[slug]/page.tsx
 
-import type { PageProps } from 'next/types'
-import { Metadata } from 'next'
-import { format } from 'date-fns'
-import { serialize } from 'next-mdx-remote/serialize'
-import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
-import MdxRenderer from '@/components/mdxRenderer'
-import { getAllPostsMeta, getPostBySlug, PostMeta } from '@/lib/posts'
+type PageProps<TParams> = { params: TParams }
 
-type Params = { slug: string }
-
-
-export function generateStaticParams(): Params[] {
-  return getAllPostsMeta().map(post => ({ slug: post.slug }))
-}
+ import type { Metadata } from 'next'
+ import { format } from 'date-fns'
+ import { serialize } from 'next-mdx-remote/serialize'
+ import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
+ import MdxRenderer from '@/components/mdxRenderer'
+ // eslint-disable-next-line @typescript-eslint/no-unused-vars
+ import { getAllPostsMeta, getPostBySlug, PostMeta } from '@/lib/posts'
 
 
-export async function generateMetadata({
+ type Params = { slug: string }
+
+ // Static parametreleri bildiriyoruz
+ export function generateStaticParams(): Params[] {
+   return getAllPostsMeta().map(post => ({ slug: post.slug }))
+ }
+
+ // Dinamik metadata (opsiyonel)
+ export async function generateMetadata({
   params
 }: PageProps<Params>): Promise<Metadata> {
-  const { meta } = getPostBySlug(params.slug)
-  return {
-    title: meta.title,
-    description: `Blog post about ${meta.tags.join(', ')}`
-  }
-}
+   const { meta } = getPostBySlug(params.slug)
+   return {
+     title: meta.title,
+     description: `Blog post about ${meta.tags.join(', ')}`
+   }
+ }
 
 export default async function PostPage({
   params
 }: PageProps<Params>) {
-  const { meta, content } = getPostBySlug(params.slug)
-  const mdxSource: MDXRemoteSerializeResult = await serialize(content)
+   const { meta, content } = getPostBySlug(params.slug)
+   const mdxSource: MDXRemoteSerializeResult = await serialize(content)
 
-  return (
-    <article className="container mx-auto flex flex-col items-center justify-center min-h-screen px-4 py-16">
-      <h1>{meta.title}</h1>
-      <time className="block text-sm text-gray-500 dark:text-gray-400 mb-6">
-        {format(new Date(meta.date), 'LLL d, yyyy')}
-      </time>
-      <MdxRenderer source={mdxSource} />
-    </article>
-  )
+   return (
+     <article className="container mx-auto flex flex-col items-center justify-center min-h-screen px-4 py-16">
+       <h1>{meta.title}</h1>
+       <time className="block text-sm text-gray-500 dark:text-gray-400 mb-6">
+         {format(new Date(meta.date), 'LLL d, yyyy')}
+       </time>
+       <MdxRenderer source={mdxSource} />
+     </article>
+   )
 }
