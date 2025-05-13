d7cc0f4b7f290c442bd3efd1941df0a717cc2214
Murathan <mrthnalgr34@gmail.com>
2025-05-13 13:20:38 +0300

    Update route.ts

--- app/api/spotify/playlists/[playlistId]/tracks/route.ts
+++ app/api/spotify/playlists/[playlistId]/tracks/route.ts
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// app/api/spotify/playlists/[playlistId]/tracks/route.ts
 import { NextResponse } from 'next/server'
 import { getPlaylistTracks } from '@/lib/spotifyService'
 
-export async function GET(
-  _request: Request,
-  { params }: { params: { playlistId: string } },
-) {
-  const data = await getPlaylistTracks(params.playlistId)
-  return NextResponse.json(data)
+
+export async function GET(request: Request, context: any) {
+  const { playlistId } = context.params as { playlistId: string }
+  try {
+    const data = await getPlaylistTracks(playlistId)
+    return NextResponse.json(data)
+  } catch (err: any) {
+    console.error('API /tracks hata:', err)
+    return NextResponse.json({ error: err.message }, { status: 500 })
+  }
 }
