3a96524c200ce70b7b06a28574e898338bfae9a0
Murathan <mrthnalgr34@gmail.com>
2025-11-13 02:11:58 +0300

    Integrate eslint-config-next with custom rules
    Added ESLint configuration for TypeScript and Next.js.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-config-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,7 +9,14 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+export default [
+  ...next(),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
