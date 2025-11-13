9b185e396d3ec801f4848dd0c9ecd483c827b612
murathanalgir <mrthnalgr34@gmail.com>
2025-11-13 04:47:55 +0300

    Update page.tsx

--- app/secretPage/page.tsx
+++ app/secretPage/page.tsx
@@ -1,17 +1,15 @@
-import GithubShowcase from "@/components/GithubShowcase";
-import Intro from "@/components/Intro";
-import Weather from "@/components/weather";
-import RandomSong from '@/components/randomSong';
+import Heart from '@/components/heart'
 
-export default function Home() {
+export default function Page() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen p-8">
-    <div className="w-full max-w-lg flex flex-col items-center space-y-12">
-      <Intro  />
-      <RandomSong />
-      <Weather />
-      <GithubShowcase />
-    </div>
-   </section>
-  );
+    <>
+      <Heart />
+    </>
+  )
 }
+
+
+
+
+
+
